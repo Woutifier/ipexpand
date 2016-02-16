95af7e39bceef8bc1d563cda0b89cee13f441b4d
Wouter de Vries <wouter@wouter-web.nl>
2016-02-16 16:23:52 +0100

    Added single column option

--- ipexpand.c
+++ ipexpand.c
@@ -2,23 +2,26 @@
 #include <string.h>
 #include <stdlib.h>
 #include <argp.h>
+#include <stdbool.h>
 
 const char *argp_program_version = "ipexpand 0.1";
 const char *argp_program_bug_address = "<w.b.devries@utwente.nl>";
 
 static char doc[] = "ipexpand -- expand csv file with ip addresses to /24's\r\nExpects an ip address in the first column and a subnet in the second";
-static char args_doc[] = "Input";
+static char args_doc[] = "inputfile";
 
 static struct argp_option options[] = {
     {"seperator", 's', "SEPERATOR", 0, "Use specified seperator for CSV input"},
     {"output", 'o', "FILE", 0, "Output to FILE instead of standard output"},
+    {"singlecolumn", 'c', 0, 0, "Read IP AND Subnet from the first column (format: ip/subnet)"},
     {0}
 };
 
 struct arguments {
     char *args[1];
-    char *seperator;
+    char *seperator;    
     char *output_file;
+    bool singlecolumn;
 };
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
@@ -28,7 +31,12 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
             arguments->output_file = arg;
             break;
         case 's':
+            if (strlen(arg) > 1)
+                argp_usage(state);
             arguments->seperator = arg;
+            break;
+        case 'c':
+            arguments->singlecolumn = true;
             break;
         case ARGP_KEY_ARG:
             if (state->arg_num >= 1)
@@ -66,24 +74,31 @@ static unsigned int ip_to_uint(char* ipstr) {
     return ip;
 }
 
-static int processline(char* line, char* seperator) {
+static int processline(char* line, char* seperator, bool singlecolumn) {
 
     char* ip = strtok(line,seperator);
-    int subnet = atoi(strtok(NULL,seperator));
-    char* rest = strtok(NULL,""); 
-    rest[strcspn(rest,"\r\n")] = 0;
-     
-    unsigned int intip = ip_to_uint(ip);
-    char* ipconv = uint_to_ip(intip);
+    int subnet;
+    char* remainder;
+    
+    if (singlecolumn ) {
+        remainder = strtok(NULL,seperator);
+        strtok(ip,"/");
+        subnet = atoi(strtok(NULL,"/"));
+    } else {
+        subnet = atoi(strtok(NULL,seperator));
+        remainder = strtok(NULL,"");
+    }
+    remainder[strcspn(remainder,"\r\n")] = 0;
+    unsigned int lowerlimit = ip_to_uint(ip)&0xffffffff<<(32-subnet);
 
     if (subnet < 24) {
-        unsigned int upperlimit = intip | 0xffffffff>>subnet;
-        for (unsigned int i = intip; i<=upperlimit; i+=0x100) {
-            printf("%s,24,%s\r\n", uint_to_ip(i),rest);
+        unsigned int upperlimit = lowerlimit | 0xffffffff>>subnet;
+        for (unsigned int i = lowerlimit; i<=upperlimit; i+=0x100) {
+            char* newip = uint_to_ip(i);
+            printf("%s%s24%s%s\r\n",newip,singlecolumn ? "/" : seperator,seperator,remainder);
+            free(newip);
         }
     }
-
-    free(ipconv);
 
     return 0;
 }
@@ -91,8 +106,11 @@ static int processline(char* line, char* seperator) {
 int main(int argc, char **argv)
 {
     struct arguments arguments;
+    
+    //Default values for arguments
     arguments.output_file = "-";
-    arguments.seperator = ",;";
+    arguments.seperator = ",";
+    arguments.singlecolumn = false;
 
     argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
@@ -107,7 +125,7 @@ int main(int argc, char **argv)
 
     while (!feof(inputFile)) {
         if (fgets(buffer, 1000, inputFile) != NULL) {
-            processline(buffer,arguments.seperator);
+            processline(buffer,arguments.seperator,arguments.singlecolumn);
         }
     }
     fclose(inputFile);
